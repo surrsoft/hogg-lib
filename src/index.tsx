a78dc1dfb5992e12bd4ec20c6e866d4ac7a017aa
Evgeny Barannikov <e.barannikov@7rlines.com>
2021-04-14 15:10:55 +0300

    rev

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 export { HoggConnectionAirtable } from './connections/HoggConnectionAirtable';
-export { HoggResult } from './utils/HoggResult';
-export { HoggOffsetCount } from './utils/HoggOffsetCount';
+
 export { HoggCellNT } from './interfaces/HoggCellNT';
 export { HoggConnectionNT } from './interfaces/HoggConnectionNT';
 export { HoggTupleNT } from './interfaces/HoggTupleNT';
+
 export { BaseCell } from './base-implements/BaseCell';
 export { BaseTuple } from './base-implements/BaseTuple';
+
+export { HoggResult } from './utils/HoggResult';
+export { HoggOffsetCount } from './utils/HoggOffsetCount';
+export { HoggErr } from './utils/HoggErr';
+export { columnNamesFrom } from './utils/hogg_utils';
